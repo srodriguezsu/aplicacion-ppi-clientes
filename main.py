cf8010731d678a80ababd7ac330d1e72c03d9482
Sebastian Rodriguez Suarez <129565458+srodriguezsu@users.noreply.github.com>
2025-02-10 16:46:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -136,6 +136,7 @@ def mapa_ubicacion(df, filtro=None, valor=None):
     gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.Longitud, df.Latitud))
     world = gpd.read_file("https://naciscdn.org/naturalearth/110m/cultural\
 /ne_110m_admin_0_countries.zip")
+    world = world[world["CONTINENT"] == "South America"]
     if filtro and valor:
         gdf = gdf[gdf[filtro] == valor]
     fig, ax = plt.subplots(figsize=(10, 6))
